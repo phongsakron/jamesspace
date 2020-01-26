feb8e6ac58e6c29f0ef53dc592f9e657d9c4ad8e
PHONGSAKON DUTSADI <59021939@up.ac.th>
2020-01-27 01:21:11 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   nameWeb = 'jamesspace.online';
 
+  @HostListener('window:scroll', ['$event'])
+  @HostListener('window:wheel', ['$event'])
+
+onWindowScroll(e) {  
+    let element = document.querySelector('.nav');
+    // console.log(window.pageYOffset, element.clientHeight);
+    
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('navbar-inverse');
+    } else {
+      element.classList.remove('navbar-inverse');
+    }
+  }
+
+  
 }
