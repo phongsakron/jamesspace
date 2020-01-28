52900ef45f265c2f7cc6065e1844f7f7451504d0
unknown <59021939@up.ac.th>
2020-01-28 16:34:38 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { HostListener } from '@angular/core';
+import * as AOS from 'node_modules/aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
   nameWeb = 'jamesspace.online';
 
   @HostListener('window:scroll', ['$event'])
-  @HostListener('window:wheel', ['$event'])
-
-onWindowScroll(e) {  
+  onWindowScroll(e) {  
     let element = document.querySelector('.nav');
     // console.log(window.pageYOffset, element.clientHeight);
     
@@ -21,6 +20,33 @@ onWindowScroll(e) {
       element.classList.remove('navbar-inverse');
     }
   }
+  ngOnInit() {
+    AOS.init({
+      // Global settings:
+      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+      initClassName: 'aos-init', // class applied after initialization
+      animatedClassName: 'aos-animate', // class applied on animation
+      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+      
+    
+      // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+      offset: 120, // offset (in px) from the original trigger point
+      delay: 200, // values from 0 to 3000, with step 50ms
+      duration: 1500, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: false, // whether elements should animate out while scrolling past them
+      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    
+    });
+  }
+
+  
+
 
   
 }
